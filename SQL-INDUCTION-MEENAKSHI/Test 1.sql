9dafe3fddf64b07c78d3a80c48f662e61120d91c
meenakshik-optimus <meenakshi.kumari@optimusinfo.com>
2015-07-13 11:48:48 +0530

    SQL TEST.

--- SQL-INDUCTION-MEENAKSHI/Test 1.sql
+++ SQL-INDUCTION-MEENAKSHI/Test 1.sql
@@ -12,6 +12,16 @@
 
 	SELECT * FROM Department;
 
+/*
+DeptID	DeptName	DeptHeadID
+1		HR				1
+2		Admin			2
+3		Sales			9
+4		Engineering		5
+
+*/
+	
+
 	--CREATION OF TABLE EMPLOYEE:-
 
 
@@ -34,6 +44,22 @@
 	
 	SELECT * FROM Employee;
 
+	/*
+
+	Id	Name	Gender	Basic	HR	DA		TAX	DeptID
+1	Anil	Male	10000	5000	1000	400	 1
+2	Sanjana	Female	12000	6000	1000	500	 2
+3	Johnny	Male	5000	2500	500		200	 3
+4	Suresh	Male	6000	3000	500		250	 1
+5	Anglia	Female	11000	5500	1000	500	 4
+6	Saurabh	Male	12000	6000	1000	600	 1
+7	Manish	Male	4000	2000	500		150	 2
+8	Neeraj	Male	5000	2500	500		200	 3
+9	Suman	Female	5000	2500	500		200	 4
+10	Tina	Female	6000	3000	500		220	 1
+
+
+*/
 	--CREATION OF TABLE EMPLOYEEATTENDANCE:-
 
 
@@ -75,25 +101,108 @@
 
 
 	SELECT * FROM EmployeeAttendance;
+	/*
+	EmpID	_Date	WorkingDays	PresentDays
+	1	2010-01-01	22				21
+	1	2010-02-01	20				20
+	1	2010-03-01	22				20
+	2	2010-01-01	22				22
+	2	2010-02-01	20				20
+	2	2010-03-01	22				22
+	3	2010-01-01	22				21
+	3	2010-02-01	20				20
+	3	2010-03-01	22				21
+	4	2010-01-01	22				21
+	4	2010-02-01	20				19
+	4	2010-03-01	22				22
+	5	2010-01-01	22				22
+	5	2010-02-01	20				20
+	5	2010-03-01	22				22
+	6	2010-01-01	22				21
+	6	2010-02-01	20				20
+	6	2010-03-01	22				20
+	7	2010-01-01	22				21
+	7	2010-02-01	20				20
+	7	2010-03-01	22				21
+	8	2010-01-01	22				21
+	8	2010-02-01	20				20
+	8	2010-03-01	22				21
+	9	2010-01-01	22				22
+	9	2010-02-01	20				20
+	9	2010-03-01	22				21
+	10	2010-01-01	22				22
+	10	2010-02-01	20				20
+	10	2010-03-01	22				22
+	*/
+
+	/*
 
 
-	--Query 1:-
+	------Query 1:-
+
+	Write a query using “Employee” and “Department” table that display gender wise number 
+	of employees in each department.
+
+	*/
+
 	 SELECT Dept.DeptName,Emp.Gender,COUNT(*) AS No_Of_Employees
 	 FROM Employee Emp INNER JOIN Department Dept 
 	 ON Emp.DeptID=Dept.DeptID GROUP BY Dept.DeptName,Emp.Gender;
 
 
-	--Query 2:-
+	 /*OUTPUT 1
 
+	 DeptName	      Gender	No_Of_Employees
+		Admin	      Female	         1
+		Engineering	  Female	         2
+		HR			  Female			 1
+		Admin		  Male				 1
+		HR			  Male	             3
+		Sales		  Male				 2
+
+*/
+
+
+
+
+
+/*
+
+	------Query 2:-
+
+	Write a query using “Employee” and “Department” table that display the results as mentioned below
+DeptName
+No. of Employee		Highest Gross Salary		Total Salary 
+--							--						--
+
+
+
+*/
 
 	SELECT Dept.DeptID,COUNT(*) AS No_Of_Employees, MAX(Emp.Basic+Emp.DA+Emp.HR) AS Highest_Gross_Salary, 
     SUM(Emp.Basic+Emp.DA+Emp.HR-Emp.TAX) AS Total_Salary
     FROM Employee Emp INNER JOIN Department Dept ON Emp.DeptID=Dept.DeptID GROUP BY Dept.DeptID;
 
+	/* OUTPUT 2
 
-	
+	DeptID	No_Of_Employees	Highest_Gross_Salary	Total_Salary
+		1		4				19000				52530
+		2		2				19000				24850
+		3		2				8000				15600
+		4		2				17500				24800
+	*/
 
-	--Query 3:-
+
+
+
+/*
+	------Query 3:-
+	Write a query using “Employee” and “Department” table that display name of the employee with the highest gross salary in each department.
+	DeptName	Name of Employee	Highest Gross Salary
+		--			--					--
+*/
+
+
 	SELECT DeptName,Name AS Name_Of_Employee,maxtable.maximum AS Highest_Gross_Salary
 	FROM Employee emp
 	INNER JOIN
@@ -106,18 +215,78 @@
 	WHERE (emp.Basic+emp.HR+emp.DA)=maxtable.maximum;
 
 
+	/* OUTPUT 3
+
+	DeptName	Name_Of_Employee	Highest_Gross_Salary
+		HR			Saurabh				19000
+		Admin		Sanjana				19000
+		Sales		Johnny				8000
+		Sales		Neeraj				8000
+		Engineering	Anglia				17500
+
+		*/
 
 
 
-	--Query 4:-
+
+/*
+	------Query 4:-
+
+	Write a query using “Employee” table that display the employee id and name whose gross salary is more than 15,000.
+
+
+*/
+
+
 	SELECT Id,Name FROM Employee WHERE (Basic+DA+HR)>15000;
 
+	/*
+	OUTPUT 4
 
-	--Query 5:-
+	Id	Name
+	1	Anil
+	2	Sanjana
+	5	Anglia
+	6	Saurabh
+
+	*/
+
+
+	/*
+
+
+
+
+	------Query 5:-
+
+	Write a query using “Employee” table that display the employee id and name who got second highest “Basic” among all employees.
+
+	*/
+
 	 SELECT Id,Name FROM employee WHERE basic=(SELECT MAX(basic) from employee WHERE  basic<(SELECT MAX(basic) FROM Employee));
 
 
-	--Query 6:-
+	 /* OUTPUT 5
+
+	 Id		Name
+	 5		Anglia
+
+*/
+
+
+
+
+/*
+
+	------Query 6:-
+	Write a query using “Employee” and “Department” table that list those departments who has number of employees more than 3.
+
+	DeptName  No. of Employees
+		--		--
+
+*/
+
+
 	SELECT X.DeptName,C.cou as No_Of_Employees FROM 
 	(SELECT Dept.DeptName DeptName,Dept.DeptID DID FROM Employee Emp INNER JOIN Department Dept ON Emp.DeptID=Dept.DeptID) X
      INNER JOIN (
@@ -127,21 +296,73 @@
     where C.cou>3
     GROUP BY X.DeptName,X.DID,C.cou;
 
+	/*
+	OUTPUT 6
+	DeptName	No_Of_Employees
+		HR			4
+
+*/
 
 
-	--Query 7:-
+
+
+/*
+	------Query 7:-
+
+	Write a query using “Employee” and “Department” table that list all the departments and their head name.
+	DeptName	Dept Head Name
+	  --				--
+*/
+
 
 	SELECT D.DeptName,E.Name FROM Employee E LEFT JOIN Department D ON D.DeptID=E.DeptID
 	WHERE D.DeptHeadID=E.Id;
 
-	--Query 8:-
+	/*
+
+	OUTPUT 7
+	DeptName	Name
+	HR			Anil
+	Admin		Sanjana
+	Engineering	Anglia
+
+*/
+
+
+
+
+
+/*
+	------Query 8:-
+	Write a query that displays the entire employees name having 100% attendance records.
+
+	*/
+
 	SELECT emp.Name AS Employee_having_full_attendance FROM Employee AS emp LEFT JOIN
 
 	(SELECT EmpID,sum(workingDays)-sum(PresentDays) AS diff FROM EmployeeAttendance GROUP BY EmpID) AS att ON emp.Id=att.EmpID WHERE diff=0;
 
+	/*
+	OUTPUT 8
+
+	Employee_having_full_attendance
+			Sanjana
+			Anglia
+			Tina
+
+*/
 
 
-	--Query 9:-
+
+
+
+/*
+	------Query 9:-
+
+	Write a query that displays the entire employees name having lowest attendance.
+*/
+
+
 	SELECT  DISTINCT Name FROM Employee emp INNER JOIN
 	EmployeeAttendance eat ON emp.Id=eat.EmpID
 	INNER JOIN(SELECT EmpID,SUM(PresentDays) AS pr
@@ -152,8 +373,29 @@
 	ON Employee.Id=e.EmpID);
 
 
-	--Query 10:-
+	/*
+	OUTPUT 9
+
+	Name
+	Anil
+	Saurabh
+*/
+
+
+
+
+	/*
+	------Query 10:-
+
+	Write a query that displays the entire employee name that has more than 3 leaves.   
+	*/
+
 	SELECT emp.Name AS employee_3_leave,emp.id,att.diff FROM Employee AS emp LEFT JOIN
 
 	(SELECT EmpID,sum(workingDays)-sum(PresentDays) AS diff FROM EmployeeAttendance GROUP BY EmpID) AS att ON emp.Id=att.EmpID WHERE diff>3;
 
+/* OUTPUT 10
+
+NO Output as leave is not greater than 3 for any employee
+
+*/  
--- SQL-INDUCTION-MEENAKSHI/Test 2.sql
+++ SQL-INDUCTION-MEENAKSHI/Test 2.sql
@@ -14,7 +14,12 @@
 	SELECT * FROM Train_Details;
 
 
-
+	/*
+	Train_ID	Train_Name
+		11404	Shatabdi
+		22505	Rajdhani
+		33606	Passenger
+*/
 
 	CREATE TABLE Station_Details(Station_ID int PRIMARY KEY,Station_Name VARCHAR(50));
 
@@ -26,7 +31,13 @@
 
 	SELECT * FROM Station_Details;
 
-
+	/*
+	Station_ID	Station_Name
+	101			Delhi
+	102			Aligarh
+	103			Lucknow
+	104			Kanpur
+*/
 
 	CREATE TABLE Journey_Details(Train_ID int FOREIGN KEY REFERENCES Train_Details(Train_ID),
 	Station_ID int FOREIGN KEY REFERENCES Station_Details(Station_ID),
@@ -63,9 +74,27 @@
 
 	 SELECT * FROM Journey_Details;
 
+	 /*
+	 Train_ID	Station_ID	Distance	Arrival_GMT 					Departure_GMT
+		11404		101			0			NULL						2012-01-25 03:00:00.000
+		11404		103			750			2012-01-25 09:30:00.000		NULL
+		22505		101			0			NULL						2012-01-25 15:04:00.000
+		22505		102			225			2012-01-25 05:30:00.000		2012-01-25 06:00:00.000
+		22505		104			150			2012-01-25 07:10:00.000		2012-01-25 07:50:00.000
+		22505		103			100			2012-01-25 08:30:00.000		NULL
+		33606		102			0			NULL						2012-01-25 10:45:00.000
+		33606		104			150			2012-01-25 13:20:00.000		2012-01-25 13:45:00.000
+		33606		103			100			2012-01-25 17:20:00.000		NULL
+*/
 
-    --Query 1:-
 
+
+/*
+    ------Query 1:-Write an SQL which gives the route map for each train and also the total
+	 distance (in KM) covered by each train and their average speed (In Km/hr) during the journey, as shown below.
+
+
+	*/
 
 	 SELECT DISTINCT T.Train_Name,q.Station_Name,q.speed,q.totaldistance FROM Train_Details T INNER JOIN
 	 (SELECT S.Station_ID,S.Station_Name,l.Train_ID,l.totaldistance,l.DiffDate,(SELECT CAST((l.totaldistance*3600.0)/l.DiffDate AS float))as speed
@@ -76,7 +105,22 @@
 	 q.totaldistance,q.speed,q.Station_Name;
 
 
-	 --Query 2A:-
+	 /* OUTPUT 1
+
+	 Train_Name	Station_Name	speed				totaldistance
+	Passenger	Aligarh			37.974683544303		250
+	Passenger	Kanpur			37.974683544303		250
+	Passenger	Lucknow			37.974683544303		250
+	Rajdhani	Aligarh			190					475
+	Rajdhani	Delhi			190					475
+	Rajdhani	Kanpur			190					475
+	Rajdhani	Lucknow			190					475
+	Shatabdi	Delhi			115.384615384615	750
+	Shatabdi	Lucknow			115.384615384615	750
+*/
+
+	 --Query 2A:-Name of the train which covered the maximum distance during its journey.
+
 
 	SELECT TOP 1 D.Train_Name FROM Train_Details D INNER JOIN Journey_Details J
 	ON D.Train_ID=J.Train_ID 
@@ -85,16 +129,26 @@
 	ON T.Train_ID=D.Train_ID
 	GROUP BY D.Train_Name ORDER BY D.Train_Name DESC;
 
+	/* OUTPUT
+	Train_Name
+	Shatabdi
+	*/
 
-	--Query 2B:-
+	--Query 2B:-Name of the train which has the max Average speed.
+
 
 	SELECT TOP 1 T.Train_Name FROM Train_Details T INNER JOIN
 	(SELECT Train_ID,SUM(Distance) 
 	AS totaldistance,DATEDIFF(SS,MIN(Departure_GMT),MAX(Arrival_GMT))
 	AS DiffDate FROM Journey_Details GROUP BY Train_ID)AS f ON T.Train_ID=f.Train_ID ORDER BY f.totaldistance/f.DiffDate;
 
+	/*
+	Train_Name
+	Rajdhani
+*/
 
-	--Query 2C:-
+	--Query 2C:-Name those trains which stop at least at three stations.
+
 
 
 	SELECT D.Train_Name FROM Train_Details D INNER JOIN Journey_Details J
@@ -106,7 +160,14 @@
     GROUP BY D.Train_Name;
 
 
-	--Query 2D:-
+	/*
+	Train_Name
+	Rajdhani
+*/
+
+
+	--Query 2D:-Name those trains whose stoppage is not Aligarh and Kanpur.
+
 
 
 	SELECT DISTINCT Train_Name
@@ -119,8 +180,32 @@
 	WHERE Station_ID IN (104)
 	GROUP BY Train_ID);
 
+	/*
+	Train_Name
+	Shatabdi
+*/
 
+/*
     --Query 4:-
+
+
+	Write an SQL query which results out the boarding and destination station name for each train as shown below.
+Train ID	Train Name	 Boarding	Destination
+111			A				Delhi	Aligarh
+222			B				Aligarh	Delhi
+
+
+*/
+
+
+
+
+ 
+
+
+
+
+
 	SELECT DISTINCT z.Train_ID,Train_Name,z.j AS Boarding,m.n AS Destination
 	FROM Train_Details
 	INNER JOIN
@@ -137,6 +222,16 @@
 	WHERE DISTANCE>0 AND Departure_GMT IS NULL) AS m
 	ON Journey_Details.Train_ID=m.Train_ID;
 
+
+	/*
+
+	OUTPUT
+
+	Train_ID	Train_Name	Boarding	Destination
+	11404		Shatabdi	Delhi		Lucknow
+	22505		Rajdhani	Delhi		Lucknow
+	33606		Passenger	Aligarh		Lucknow
+*/
 
 	--Query 5:-
 
@@ -185,12 +280,10 @@
 	);
 
 
-	SELECT * FROM Departments ;
-
-	SELECT * FROM Projects;
-	SELECT * FROM Engineers;
-	SELECT * FROM Attendance;
-
+	
+	
+	
+	
 
 	INSERT INTO Departments
 	(Dept_Id ,
@@ -200,6 +293,13 @@
 	(2,'Development'),
 	(3,'Testing');
 
+	SELECT * FROM Departments ;
+	/*
+	Dept_Id		Dept_Name
+	1			HR
+	2			Development
+	3			Testing
+*/
 
 	INSERT INTO Projects
 	(P_Id,P_Name,Dept_Id)
@@ -212,6 +312,17 @@
 	(6,'snapdeal','2'),
 	(7,'nokia','3');
 
+	SELECT * FROM Projects;
+	/*
+	P_Id		P_Name			Dept_Id
+	1			Monkey_media	2
+	2			Amazon			2
+	3			(x)matters		2
+	4			samsung			3
+	5			Olacabs			3
+	6			snapdeal		2
+	7			nokia			3
+*/
 
 	INSERT INTO Engineers
 	(E_Id ,
@@ -225,6 +336,18 @@
 	(5,2,'Manish'),
 	(6,3,'Nitish'),
 	(7,3,'sarika');
+
+	SELECT * FROM Engineers;
+	/*
+	E_Id	Dept_Id		Emp_Name
+	1		2			Shaumya
+	2		3			Meena
+	3		3			Adam
+	4		2			Jack
+	5		2			Manish
+	6		3			Nitish
+	7		3			sarika
+*/
 
 
 	INSERT INTO Attendance
@@ -241,8 +364,21 @@
 	(4,3,3,'2010-2-5',9),
 	(5,3,3,'2010-2-5',4);
 
-	--Query 2(a):-
+	SELECT * FROM Attendance;
+	/*
+	Att_Id	E_Id	P_Id	Atten_Date	Hrs
+	1		1		1		2010-02-05	7
+	2		2		2		2010-02-05	6
+	3		2		2		2010-02-05	11
+	4		3		3		2010-02-05	9
+	5		3		3		2010-02-05	4
+*/
 
+/*	--Query 2(a):-
+
+	Query which displays the total number of hours spend as Each engineer in their respective project.
+
+*/
 
 	SELECT engr.Emp_Name,sum(att.Hrs) as TOTAL_HOURS
 	from
@@ -250,12 +386,37 @@
 	ON engr.E_Id=att.E_Id
 	GROUP BY engr.Emp_Name;
 
-
+/*
+	OUTPUT
+	Emp_Name	TOTAL_HOURS
+	Adam		13
+	Jack		NULL
+	Manish		NULL
+	Meena		17
+	Nitish		NULL
+	sarika		NULL
+	Shaumya		7
+*/
+	
+	/*
 	--Query 2(b):-
-
+	Query which displays the total number of hours spend as 
+	Each project in their respective department.
+	*/
 
 	SELECT pr.P_Name,sum(att.Hrs) as TOTAL_HRS_PROJECT
 	from
 	Projects pr LEFT JOIN Attendance att
 	ON pr.P_Id=att.P_Id
 	GROUP BY pr.P_Name;
+
+	/* OUTPUT
+	P_Name			TOTAL_HRS_PROJECT
+	(x)matters		13
+	Amazon			17
+	Monkey_media	7
+	nokia			NULL
+	Olacabs			NULL
+	samsung			NULL
+	snapdeal		NULL
+*/
